d980a287e127caf81f97cd194e051be237a7d392
Clatirissa Batista <clarissapbatista@gmail.com>
2023-03-09 15:48:35 +0000

    category backend done

--- app/controllers/user_categories_controller.rb
+++ app/controllers/user_categories_controller.rb
@@ -4,21 +4,12 @@ class UserCategoriesController < ApplicationController
   end
 
   def create
-
+    @category = Category.find(params[:user_category][:category])
+    @user_category = UserCategory.new(category: @category, user: current_user)
+    if @user_category.save
+      redirect_to polls_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
-
-  private
-
-  def user_category_params
-    params.require(:user_category).permit(:poll, :user)
-  end
-end
-
-
-@user_categories = UserCategory.new(user_category_params)
-if @user_category.select?
-  redirect_to polls_path(@user_category)
-
-else
-  render :new, status: :unprocessable_entity
 end
