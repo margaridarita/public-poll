e1f7996b14a8d51e51d89d9492bf4a29696faee2
Sofia Magalhaes Clara <sofiamfclara@gmail.com>
2023-03-14 17:51:16 +0000

    cleaned photo logic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def user_photo(user, options = {})
+    default_url = "https://cdn-icons-png.flaticon.com/512/2119/2119043.png"
+
+    if user.photo.attached?
+      cl_image_tag(user.photo.key, options)
+    else
+      image_tag(default_url, options)
+    end
+  end
 end
