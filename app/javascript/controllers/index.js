f3c2b5da7bf96eb1e53d9bf64d00739bff52cb3f
margarida rita <margaridabr@outlook.pt>
2023-03-14 11:51:53 +0000

    fixed js

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -15,9 +15,3 @@ application.register("poll", PollController)
 
 import UserController from "./user_controller"
 application.register("user", UserController)
-
-import { Application } from '@hotwired/stimulus'
-import ScrollTo from 'stimulus-scroll-to'
-
-const application = Application.start()
-application.register('scroll-to', ScrollTo)
