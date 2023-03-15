b87eea802b8d5834e6cf66aebdd6a979126ff026
Sofia Magalhaes Clara <sofiamfclara@gmail.com>
2023-03-15 16:54:46 +0000

    fix delete logic for friendships

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_13_182009) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_15_161324) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -87,6 +87,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_13_182009) do
     t.string "second_option"
     t.bigint "category_id", null: false
     t.integer "votes_count", default: 0
+    t.boolean "private", default: false
     t.index ["category_id"], name: "index_polls_on_category_id"
     t.index ["user_id"], name: "index_polls_on_user_id"
   end
