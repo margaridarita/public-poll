66d48f85c512488b2357955b213bb1a38fc8598b
Sofia Magalhaes Clara <sofiamfclara@gmail.com>
2023-03-16 10:24:29 +0000

    fixed fyp logic if no friends

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -8,12 +8,18 @@ class PollsController < ApplicationController
       @search = Search.where(query: params[:query], user: current_user).first_or_create
       @search.touch
     else
-      @polls = Poll.joins("INNER JOIN friendships ON (friendships.to_user_id = polls.user_id OR friendships.from_user_id = polls.user_id)")
-                   .where("friendships.to_user_id = ? OR friendships.from_user_id = ?", current_user, current_user)
-                   .where.not("polls.user_id = ?", current_user)
-                   .or(Poll.where(user_id: current_user))
-                   .or(Poll.where(private: false))
-                   .order(created_at: :desc).distinct
+      if current_user.friendships.empty?
+        @polls = Poll.where(user_id: current_user)
+                     .or(Poll.where(private: false))
+                     .order(created_at: :desc).distinct
+      else
+        @polls = Poll.joins("INNER JOIN friendships ON (friendships.to_user_id = polls.user_id OR friendships.from_user_id = polls.user_id)")
+                     .where("friendships.to_user_id = ? OR friendships.from_user_id = ?", current_user, current_user)
+                     .where.not("polls.user_id = ?", current_user)
+                     .or(Poll.where(user_id: current_user))
+                     .or(Poll.where(private: false))
+                     .order(created_at: :desc).distinct
+      end
     end
   end
 
